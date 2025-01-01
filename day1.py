7e67f88d23cd0663805a7c88a68203cb8a6534ef
syskey8 <129398699+syskey8@users.noreply.github.com>
2025-01-01 10:30:22 +0530

    day 1 notes

---
+++ day1.py
@@ -0,0 +1,15 @@
+# print function
+
+print("Hello world!") # prints the string
+print("Hello\nWorld") # prints new line at \n
+print("Hello" + "Angela") # concatenates strings, output: HelloAngela
+print("Hello" + " " + "Angela") # output: Hello Angela
+input("What is your name?") # takes input from user
+print("Hello " + input("What is your name?")) # example
+
+# variables
+name = input("what is your name?") # if input = jack
+print(name) # output: jack
+
+# len function : gives the length of the string
+print(len(("What is your name:"))) # output: 18
