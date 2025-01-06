81edc7a97ffe6e193327ba976e995b96d1e9c489
syskey8 <129398699+syskey8@users.noreply.github.com>
2025-01-06 08:27:43 +0530

    day 6 notes

---
+++ day6.py
@@ -0,0 +1,16 @@
+# function
+def my_function():  # defination of function
+    print("Hello")
+    print("bye")
+
+my_function() # calling the function (executing the function)
+
+# while loop
+
+# Initialize the counter
+counter = 1
+
+# While loop condition
+while counter <= 5:
+    print("Counter value:", counter)
+    counter += 1  # Increment the counter
