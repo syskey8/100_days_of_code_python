97aff4fda10814b4dcf3ed0cadf024f8d7ceeaf4
syskey8 <129398699+syskey8@users.noreply.github.com>
2025-01-22 18:15:37 +0530

    day 22 notes

---
+++ day22.py
@@ -0,0 +1,15 @@
+# TODO Create the screen
+
+# TODO Create and move a paddle
+
+# TODO Create another paddle
+
+# TODO Create the ball and make it move
+
+# TODO Detect collision with wall and bounce
+
+# TODO Detect collision with paddle
+
+# TODO Detect when paddle misses
+
+# TODO Keep score
